5355b9808b88648e47289c05adbc589a7f68fc89
Jakub Rodak <34693@student.pwsztar.edu.pl>
2022-04-29 17:01:26 +0200

    zad1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import QuizComponent from "./components/quiz-component";
+import Question from "./components/questions";
+
+function App() {
+  return (
+    <div className="App">
+      <QuizComponent />
+    </div>
+  );
+}
+
+export default App;
